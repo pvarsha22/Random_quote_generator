037d7d4aff6b060770be658c90945aae88cf9026
pvarsha22 <varshapatil498@gmail.com>
2020-11-19 11:00:41 +0530

    add initial files

---
+++ quote.js
@@ -0,0 +1,37 @@
+const arrayQuotes=[
+    {
+        'author':'Jim Rohn',
+        'quote':'Beware of what you become in pursuit of what you want...'
+    },
+    {
+        'author':'Epictetus',
+        'quote':'It\'s not what happents to you, but how react to it matters...  '
+    },
+    {
+        'author':'Frank Sinatra',
+        'quote':'The best revenge is massive success...'
+    },
+    {
+        'author':'Wayne Gretzy',
+        'quote':'You miss 100% of the shots you don\'t take...'
+    },
+    {
+        'author':'Nelson Mandela',
+        'quote':'Resenment is like drinking poison and waiting for your enemies to die...'
+    },
+    {
+        'author':'Elbert Hubbard',
+        'quote':'Do not take life seriously. You will not get out alive...'
+    },
+    {
+        'author':'William James',
+        'quote':'Act as if what you do makes a difference..It does...'
+    }
+];
+
+
+function generateQuote(){
+    const ran= Number.parseInt(Math.random()*arrayQuotes.length+1);
+    document.querySelector('#quoteOutput').textContent =arrayQuotes[ran].quote;
+    document.querySelector('#authorOutput').textContent =arrayQuotes[ran].author;
+}
