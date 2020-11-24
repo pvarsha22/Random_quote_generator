d468dafe0b01c1ea28de4ffd68ebd71f5086dd1e
pvarsha22 <varshapatil498@gmail.com>
2020-11-24 13:48:20 +0530

    changed functionality

--- quote.js
+++ quote.js
@@ -43,7 +43,7 @@ const arrayQuotes=[
 
 
 function generateQuote(){
-    const ran= Number.parseInt(Math.random()*arrayQuotes.length);
+    const ran= Math.floor(Math.random()*arrayQuotes.length+1);
     document.querySelector('#quoteOutput').textContent ="\"" + arrayQuotes[ran].quote + "\"";
     document.querySelector('#authorOutput').textContent ="--" + arrayQuotes[ran].author;
 }
